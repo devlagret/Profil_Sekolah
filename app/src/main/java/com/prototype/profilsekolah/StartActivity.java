6c8e5d8240733f82b3bc8576aaa1e106900dca61
sioputra <sioputra4@gmail.com>
2022-03-22 06:52:26 +0700

    Intial commit

---
+++ app/src/main/java/com/prototype/profilsekolah/StartActivity.java
@@ -0,0 +1,35 @@
+package com.prototype.profilsekolah;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class StartActivity extends AppCompatActivity {
+private static int timer=9000;
+
+    View ivMenu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        ivMenu = findViewById(R.id.iv_menu);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },timer);
+        ivMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }}
